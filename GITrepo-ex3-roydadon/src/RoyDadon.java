190685decf06db180e0e7ac247a06420218c724d
rroyd <92038509+rroyd@users.noreply.github.com>
2025-08-18 00:00:26 +0300

    Updated Hello

--- GITrepo-ex3-roydadon/src/RoyDadon.java
+++ GITrepo-ex3-roydadon/src/RoyDadon.java
@@ -1,7 +1,7 @@
 public class RoyDadon {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
-            System.out.println("Roy The King");
+            System.out.println("Roy Ha Melech");
         }
     }
 }
