c97d814af0264fa20d818989f853cd1dbba3d878
rroyd <92038509+rroyd@users.noreply.github.com>
2025-08-17 23:53:57 +0300

    Updated Hello

--- GITrepo-ex3-roydadon/src/Hello.java
+++ GITrepo-ex3-roydadon/src/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!\n");
 
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 15; i++) {
             System.out.println("i = " + i);
         }
     }
